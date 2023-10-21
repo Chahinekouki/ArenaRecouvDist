076de2347bd8cccb67e2f4905b5e2de27c369029
assylkriaa <assyl.kriaa@esprit.com>
2023-10-21 16:43:50 +0100

    api-commit

---
+++ APIGateway/src/main/java/com/esprit/getaway/ApiGatewayApplication.java
@@ -0,0 +1,28 @@
+package com.esprit.getaway;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("RecouvGTI", r -> r.path("/RecouvGTI/**", "/dossier", "/getalldossier")
+						.uri("http://localhost:8081/"))
+				.build();
+
+	}
+
+}
